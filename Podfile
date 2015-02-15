20b412ecd4f0b10307eabee9aab1b3298f96db05
kevin chen <ksrchen@gmail.com>
2015-02-15 08:39:13 -0800

    add pods

---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment this line to define a global platform for your project
+#platform :ios, '8.0'
+
+target 'Multi-Family Properties Search' do
+pod 'AFNetworking', '2.2.1'
+end
+
---
+++ Pods/Target Support Files/Pods-Multi-Family Properties Search/Pods-Multi-Family Properties Search-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Multi_Family_Properties_Search : NSObject
+@end
+@implementation PodsDummy_Pods_Multi_Family_Properties_Search
+@end
