88850737304017ae0a85c262338dee18be9aa4ba
kevin chen <kevichen@kevis-Mac.local>
2015-04-09 13:34:37 -0700

    add add property view

---
+++ Multi-Family Properties Search/AddPropertyViewController.h
@@ -0,0 +1,14 @@
+//
+//  AddPropertyViewController.h
+//  Chrometopia
+//
+//  Created by kevin chen on 4/9/15.
+//  Copyright (c) 2015 Kevin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddPropertyViewController : UITableViewController
+
+- (IBAction)saveButtonTap:(id)sender;
+@end
